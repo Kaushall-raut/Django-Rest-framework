4c142ed14deebb67e38e45367eeedfead24ba9f2
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-05-27 21:32:43 +0530

    rest framework get

--- restGET/api/admin.py
+++ restGET/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Students
 # Register your models here.
+
+@admin.register(Students)
+class AdminStudent(admin.ModelAdmin):
+    list_display=['name','city']
--- restGET/api/views.py
+++ restGET/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Students
+from .searializer import StudentSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def home(request):
+    data=Students.objects.all()
+    serializer=StudentSerializer(data,many=True)
+    return Response(serializer.data)
