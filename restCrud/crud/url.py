4f2d10da7dd597fc2ff3bc8398cbb3caf18f479c
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-05-28 15:53:17 +0530

    post api

--- restCrud/crud/url.py
+++ restCrud/crud/url.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("get",views.getApi, name='get')
+    path("get",views.getApi, name='get'),
+    path("post",views.postApi, name='post')
 ]
--- restCrud/crud/views.py
+++ restCrud/crud/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Student
 from .serializer import StudentSerializer
+from rest_framework import status
 # Create your views here.
 
 @api_view(['GET'])
@@ -10,3 +11,13 @@ def getApi(request):
    data=Student.objects.all()
    serializer=StudentSerializer(data,many=True)
    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def postApi(request):
+   serializer=StudentSerializer(data=request.data)
+
+   if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data ,status=status.HTTP_201_CREATED)
+   return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
